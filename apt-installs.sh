dc564262952ae608f9a74f779a1b3911fc70adb2
nick <nickkiermaier@gmail.com>
2015-08-30 14:02:15 -0500

    updates

--- apt-installs.sh
+++ apt-installs.sh
@@ -2,20 +2,18 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-#dev folder
-mkdir ~/dev
 
 # basic environment 
 # -------------------------------------------------
-sudo apt-get install git
-sudo apt-get install curl
-sudo apt-get install htop
-sudo apt-get install vlc
-sudo apt-get install p7zip p7zip-rar
-sudo apt-get install gimp
+#sudo apt-get install git
+#sudo apt-get install curl
+#sudo apt-get install htop
+#sudo apt-get install vlc
+#sudo apt-get install p7zip p7zip-rar
+#sudo apt-get install gimp
 
 # optional
-# sudo apt-get install blender
+sudo apt-get install blender
 # figure out utorrent @ some point
 
 
@@ -36,43 +34,5 @@ sudo apt-get install apache2
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
-
-
-
-# dev stuffs
-# -----------------------------------------------
-# python dev
-# easy_install:
-sudo apt-get install python-setuptools
-# ipython:
-sudo easy_install ipython
-
-
-#javascript dev
-# npm:
-# sudo apt-get install npm
-# yeoman:
-# npm install -g yo
-
-
-
-
-# ruby dev ?
-# sudo apt-get install ruby
-
-
-
-
-# gems
-# ----------------------------
-# openshift command line tools
-# gem install rhc
-# rails
-# passenger
-
-
-
-
-
 
 
---
+++ dev-installs.sh
@@ -0,0 +1,62 @@
+# dev stuffs
+# -----------------------------------------------
+
+#dev folder
+mkdir ~/dev
+
+# python dev
+#easy_install:
+sudo apt-get install python-setuptools
+# ipython:
+sudo easy_install ipython
+
+
+# ruby dev ?
+#note: may need to chown the .gnupg folder in ~
+# must also make the command prompt  a login shell
+# edit ->profile prefs->command->make prompt login shell
+
+# add the rvm dev's public key
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+# install rvm with rails
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+
+# back up system files before messing with
+# add a date to file name at some point
+sudo cp .bash_profile .bash_profile.bkp
+sudo cp .bashrc .bashrc.bkp
+
+# add rvm source to both of them
+# may need to chown them if not working
+sudo echo "source ~/.rvm/scripts/rvm" >> .bash_profile
+sudo echo "source ~/.rvm/scripts/rvm" >> .bashrc
+
+
+
+
+
+#javascript dev
+# npm:
+#sudo apt-get install npm
+# yeoman:
+# npm install -g yo
+# see this: https://github.com/sindresorhus/guides/blob/master/npm-global-without-sudo.md
+
+
+
+
+
+# gems
+# ----------------------------
+# openshift command line tools
+# gem install rhc
+#passenger 
+# gem install passenger
+
+
+
+
+
+
+
