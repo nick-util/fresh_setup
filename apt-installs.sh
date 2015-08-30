68f5dc8b8863db32af23d9d4e423ba81acc9f928
Nick Kiermaier <nickkiermaier@gmail.com>
2015-08-30 15:21:00 -0500

    Update apt-installs.sh

--- apt-installs.sh
+++ apt-installs.sh
@@ -1,38 +1,38 @@
 # updates
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
 
 # basic environment 
 # -------------------------------------------------
-sudo apt-get install git
-sudo apt-get install curl
-sudo apt-get install htop
-sudo apt-get install vlc
-sudo apt-get install p7zip p7zip-rar
-sudo apt-get install gimp
-sudo apt-get install libmysqlclient-dev
-sudo apt-get install libpq-dev
+sudo apt-get -y install git
+sudo apt-get -y install curl
+sudo apt-get -y install htop
+sudo apt-get -y install vlc
+sudo apt-get -y install p7zip p7zip-rar
+sudo apt-get -y install gimp
+sudo apt-get -y install libmysqlclient-dev
+sudo apt-get -y install libpq-dev
 
 
 
 # chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update 
-sudo apt-get install google-chrome-stable
+sudo apt-get -y update 
+sudo apt-get -y install google-chrome-stable
 
 # mysql
-sudo apt-get install mysql-server
-sudo apt-get install mysql-client
+sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-client
 
 # apache
-sudo apt-get install apache2
+sudo apt-get -y install apache2
 
 # sublime
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get -y update
+sudo apt-get -y install sublime-text-installer
 
 # optional
 # sudo apt-get install blender
