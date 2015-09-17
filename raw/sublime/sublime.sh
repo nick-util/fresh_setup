36960d899fdc4c51cb00c7f3d1e0345923616811
Nick Kiermaier <nickkiermaier@gmail.com>
2015-09-17 18:47:34 -0500

    Update sublime.sh

--- raw/sublime/sublime.sh
+++ raw/sublime/sublime.sh
@@ -1,10 +1,10 @@
 # install package control
-curl https://packagecontrol.io/Package%20Control.sublime-package > ~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package
+bash <(curl https://packagecontrol.io/Package%20Control.sublime-package > ~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package )
 
 # copy package settings file
-curl https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/raw/sublime/config_files/Package%20Control.sublime-settings \
- > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+bash <(curl https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/raw/sublime/config_files/Package%20Control.sublime-settings \
+ > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings)
 # copy user settings file
-curl  https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/raw/sublime/config_files/Preferences.sublime-settings >  ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+bash <(curl  https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/raw/sublime/config_files/Preferences.sublime-settings >  ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings)
 
 
