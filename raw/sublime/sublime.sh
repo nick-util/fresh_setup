5e8a21be7d982cbb20a9e70fed8c04dd4443b23d
Nick Kiermaier <nickkiermaier@gmail.com>
2015-09-17 14:54:16 -0500

    Update sublime.sh

--- raw/sublime/sublime.sh
+++ raw/sublime/sublime.sh
@@ -2,9 +2,9 @@
 curl https://packagecontrol.io/Package%20Control.sublime-package > ~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package
 
 # copy package settings file
-curl https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/Package%20Control.sublime-settings > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
-
+curl https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/raw/sublime/config_files/Package%20Control.sublime-settings
+ > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
 # copy user settings file
-curl https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/Preferences.sublime-settings >  ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+curl  https://raw.githubusercontent.com/nkiermaier/fresh-setup/master/raw/sublime/config_files/Preferences.sublime-settings >  ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 
 
